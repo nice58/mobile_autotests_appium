a8ab92bb1bdd4ad82bfcdbedc4d7a01c3d72038e
n.trubanenko <n.trubanenko@evotor.ru>
2023-11-23 22:41:20 +0300

    after code review

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,15 +1,20 @@
 package helpers;
 
+import config.UserConfig;
+import org.aeonbits.owner.ConfigFactory;
+
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
 public class Browserstack {
 
+    static UserConfig userConfig = ConfigFactory.create(UserConfig.class, System.getProperties());
+
     public static String videoUrl(String sessionId) {
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
-                .auth().basic("natalia_7PX5eh", "BH7rBcGmzSL6scgY6os5")
+                .auth().basic(userConfig.getUserName(), userConfig.getUserPassword())
                 .when()
                 .get(url)
                 .then()
